a6d7cd3ca8fbd210ae261d94fff6205332821373
Hayk-Grigoryan-92 <hayk.grigoryan992@gmail.com>
2023-05-22 02:13:37 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// Task 1
+
+// let array=[1,4,8,5,6];
+// console.log(array);
+
+// function deleteElement(arr){
+//     let result = []
+
+//     if(arr.length == 1){
+//         return console.log(result);
+//     }
+//     if(!arr.length){
+//         return console.log(result);
+//     }
+//     if(arr.length>1){
+//         arr[0] = arr[1]
+//         arr.shift()
+//        console.log(arr);
+//     }
+//     deleteElement(arr)
+//  }
+
+//  deleteElement(array)
+
+ ///////////////////////////////////////////////  aranc shift() chem karoxacel
+
+ // Task 3
+
+//  function sumNumberElement (num){
+
+//     let arrString = Array.from(String(num) )
+//     console.log(arrString);
+//     let arrNumber = Array.from(String(num), Number)
+//     console.log(arrNumber);
+
+//     let sum = 0
+//     for(let i = 0; i < arrNumber.length; i++){
+//         sum +=arrNumber[i] 
+//     }
+//     console.log(sum);   
+    
+//     if(sum>9){
+//         sumNumberElement(sum)
+//     }
+    
+// }
+
+//  sumNumberElement(1256)
+
+ //////////////////////////////////////////////////////////////////
+
+
